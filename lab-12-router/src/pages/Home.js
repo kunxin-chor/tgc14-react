ec822d8e0c948558a08d3e45cd4efa6ea0ca016c
kunxin-chor <chorkunxin@yahoo.com>
2021-11-11 04:39:31 +0000

    Added useEffect with useParam example

--- lab-12-router/src/pages/Home.js
+++ lab-12-router/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
 
 export default function Home() {
     return (
         <React.Fragment>
             <h1>Home Page</h1>
+            <Link to="/posts/3">Post ID 3</Link>
         </React.Fragment>
     )
 }
